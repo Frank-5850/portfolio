6e67aca7b2520710674ec35609a5aec1337d0543
franco <phanfranco@yahoo.com>
2022-06-30 11:54:40 -0700

    add: first project

--- src/Work/Work.js
+++ src/Work/Work.js
@@ -1,12 +1,57 @@
 import React from "react";
-import { WorkContainer, WorkWrapper } from "./work.styles";
+import {
+  GitHubIcon,
+  Language,
+  LanguageAndLinks,
+  LanguageList,
+  WebsiteIcon,
+  WorkCard,
+  WorkContainer,
+  WorkDetails,
+  WorkImg,
+  WorkInfo,
+  WorkLink,
+  WorkLinkContainer,
+  WorkTitle,
+  WorkWrapper,
+} from "./work.styles";
 import weatherApp from "../assets/Weather.jpg";
 
 const Work = () => {
   return (
     <WorkWrapper>
       <WorkContainer>
-        <img src={weatherApp} alt="" />
+        <WorkCard>
+          <WorkImg src={weatherApp} alt="Weather app photo" />
+          <WorkInfo>
+            <WorkTitle>Weather app</WorkTitle>
+            <WorkDetails>
+              Simple weather application build using the Open-Weather API. Users
+              are able to get the weather, 24 hour forecast and 7 day forecast
+              for their current location or search a location.
+            </WorkDetails>
+            <LanguageAndLinks>
+              <LanguageList>
+                <Language>react</Language>
+                <Language>node</Language>
+                <Language>express</Language>
+                <Language>styled-components</Language>
+                <Language>open weather map API</Language>
+              </LanguageList>
+              <WorkLinkContainer>
+                <WorkLink>
+                  <GitHubIcon />
+                </WorkLink>
+                <WorkLink>
+                  <WebsiteIcon />
+                </WorkLink>
+              </WorkLinkContainer>
+            </LanguageAndLinks>
+          </WorkInfo>
+        </WorkCard>
+        {/* <WorkCard />
+        <WorkCard />
+        <WorkCard /> */}
       </WorkContainer>
     </WorkWrapper>
   );
--- src/Work/work.styles.js
+++ src/Work/work.styles.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { FaGithubSquare } from "react-icons/fa";
+import { MdOpenInNew } from "react-icons/md";
 
 export const WorkWrapper = styled.div`
   display: flex;
@@ -14,13 +16,92 @@ export const WorkWrapper = styled.div`
 
 export const WorkContainer = styled.div`
   display: flex;
-  flex-direction: column;
-  height: 50vh;
-  width: 50vw;
+  flex-flow: column wrap;
+  height: 75vh;
+  width: 66vw;
+  // border: 1px solid red;
   @media (min-width: 426px) and (max-width: 1024px) {
     width: 90vw;
   }
   @media (min-width: 100px) and (max-width: 425px) {
     width: 100vw;
+  }
+`;
+
+export const WorkCard = styled.div`
+  display: flex;
+  flex-flow: row;
+  // height: 25%;
+  width: 100%;
+  // border: 1px solid blue;
+`;
+
+export const WorkImg = styled.img`
+  width: 30%;
+`;
+
+export const WorkInfo = styled.div`
+  padding-left: 10px;
+  width: 70%;
+`;
+
+export const WorkTitle = styled.h2`
+  margin: 0;
+  padding: 0;
+  margin: 0 0 5px 0;
+`;
+
+export const WorkDetails = styled.p`
+  margin: 0;
+  padding: 0;
+  margin: 0 0 5px 0;
+`;
+
+export const LanguageAndLinks = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-flow: row wrap;
+  height: 50%;
+`;
+
+export const LanguageList = styled.ul`
+  display: flex;
+  flex-flow: column;
+`;
+
+export const Language = styled.li``;
+
+export const WorkLinkContainer = styled.div`
+  display: flex;
+  // flex-direction: row;
+  // justify-content: flex-end;
+  @media (min-width: 100px) and (max-width: 425px) {
+    background-color: #6c757d;
+  }
+`;
+
+export const WorkLink = styled.a`
+  text-decoration: none;
+  margin: 0 5px;
+  @media (min-width: 100px) and (max-width: 425px) {
+    background-color: #6c757d;
+  }
+`;
+
+export const GitHubIcon = styled(FaGithubSquare)`
+  color: white;
+  font-size: 1.5em;
+  margin: 0 5px;
+  @media (min-width: 100px) and (max-width: 425px) {
+    background-color: #6c757d;
+  }
+`;
+
+export const WebsiteIcon = styled(MdOpenInNew)`
+  color: white;
+  font-size: 1.5em;
+  margin: 0 5px;
+  @media (min-width: 100px) and (max-width: 425px) {
+    background-color: #6c757d;
   }
 `;
