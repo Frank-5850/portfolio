e26021ef25240bb49b017127e65211ed5951a9b4
Franco <phanfranco@yahoo.com>
2022-06-29 23:17:13 -0700

    add: social links

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import LandingPage from "./LandingPage/LandingPage";
 import AboutMe from "./AboutMe/AboutMe";
 import Contact from "./Contact/Contact";
+import Socials from "./Socials/Socials";
 
 function App() {
   return (
     <div className="App">
+      <Socials />
       <LandingPage />
       <AboutMe />
       <Contact />
---
+++ src/Socials/socials.styles.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const SocialsWrapper = styled.div`
+  width: 40px;
+  position: fixed;
+  bottom: 0px;
+  left: 40px;
+  right: auto;
+  z-index: 10;
+`;
+
+export const SocialsContainer = styled.ul`
+  display: flex;
+  flex-direction: column;
+  -webkit-box-align: center;
+  align-items: center;
+  margin: 0px;
+  padding: 0px;
+  list-style: none;
+  &:after {
+    content: "";
+    display: block;
+    width: 1px;
+    height: 300px;
+    margin: 0px auto;
+    background-color: white;
+  }
+`;
+
+export const IconBox = styled.li`
+  padding: 5px;
+`;
+
+export const IconLink = styled.a`
+  color: white;
+  text-decoration: none;
+  &:hover {
+    color: #f5f5f5;
+  }
+`;
